c543eaa8ee32c69e2ff1bb59c22d9d72bc04e524
Dániel Szilágyi <szilagyi.daniel88@gmail.com>
2024-07-03 12:03:42 +0200

    Add MV04 state handler class library

---
+++ ExtLibs/MV04.State/StateHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MV04.State
+{
+    public enum MV04_State
+    {
+        Manual,
+        TapToFly,
+        Auto,
+        Follow,
+        FPV,
+        Takeoff,
+        RTL,
+        Land,
+        Unknown
+    }
+
+    public class MV04StateChangeEventArgs: EventArgs
+    {
+        public MV04_State PreviousState { get; set; }
+
+        public MV04_State NewState { get; set; }
+
+        public MV04StateChangeEventArgs(MV04_State previousState, MV04_State newState)
+        {
+            PreviousState = previousState;
+            NewState = newState;
+        }
+    }
+
+    public static class StateHandler
+    {
+        private static MV04_State _currentSate;
+
+        public static MV04_State CurrentSate
+        {
+            get => _currentSate;
+            set
+            {
+                MV04_State previousState = _currentSate;
+                _currentSate = value;
+                MV04StateChange(null, new MV04StateChangeEventArgs(previousState, _currentSate));
+            }
+        }
+
+        public static event EventHandler<MV04StateChangeEventArgs> MV04StateChange;
+    }
+}
