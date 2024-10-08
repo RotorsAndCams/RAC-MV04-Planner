f41a591f6b172e79603915a36c8214e9e1b82c9c
venczakt <tamas.venczak@rotorsandcams.com>
2024-10-08 17:22:58 +0200

    get user name

---
+++ GCSViews/GetUserNameForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MissionPlanner.GCSViews
+{
+    partial class GetUserNameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_Name = new System.Windows.Forms.TextBox();
+            this.lb_Name = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_Name
+            // 
+            this.tb_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tb_Name.Location = new System.Drawing.Point(177, 12);
+            this.tb_Name.Name = "tb_Name";
+            this.tb_Name.Size = new System.Drawing.Size(332, 26);
+            this.tb_Name.TabIndex = 0;
+            this.tb_Name.Text = "Enter your name and badge number";
+            // 
+            // lb_Name
+            // 
+            this.lb_Name.AutoSize = true;
+            this.lb_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lb_Name.Location = new System.Drawing.Point(12, 15);
+            this.lb_Name.Name = "lb_Name";
+            this.lb_Name.Size = new System.Drawing.Size(123, 16);
+            this.lb_Name.TabIndex = 1;
+            this.lb_Name.Text = "Name / Badge num";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // GetUserNameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(521, 109);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb_Name);
+            this.Controls.Add(this.tb_Name);
+            this.Name = "GetUserNameForm";
+            this.Text = "GetUserNameForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_Name;
+        private System.Windows.Forms.Label lb_Name;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ GCSViews/GetUserNameForm.cs
@@ -0,0 +1,73 @@
+﻿using MissionPlanner.Utilities;
+using MV04.Settings;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MissionPlanner.GCSViews
+{
+    public partial class GetUserNameForm : Form
+    {
+        private const string PilotDataFileName = "PilotLog.json";
+
+        public GetUserNameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = this.tb_Name.Text;
+
+            if (name.Length < 3)
+            {
+                MessageBox.Show("Enter a valid name");
+                return;
+            }
+
+            //var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+
+
+
+            //check valid text
+            //save text to xml
+
+            Save();
+            this.Close();
+
+        }
+
+        private void Save()
+        {
+            var fileToWrite = MissionPlanner.Utilities.Settings.GetUserDataDirectory() + PilotDataFileName;
+
+            //if (!File.Exists(fileToWrite))
+            //{ 
+            //    File.Create(fileToWrite);
+            //}
+
+            var lst = JsonConvert.DeserializeObject<List<PilotUserLog>>(File.ReadAllText(fileToWrite)) ?? new List<PilotUserLog>();
+
+            lst.Add(new PilotUserLog() { Name = this.tb_Name.Text, Datetime = DateTime.Now.ToString("yyyyMMddHHmmss") });
+
+            
+
+            File.WriteAllText(fileToWrite, lst.ToJSON());
+        }
+    }
+
+    public class PilotUserLog
+    {
+        public string Name;
+        public string Datetime;
+    }
+}
