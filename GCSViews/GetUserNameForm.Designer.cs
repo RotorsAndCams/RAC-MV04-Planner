9cf2b12e00ab471c8bffd6d10fd59023aeb1c075
Dániel Szilágyi <17363065+LEGIONBOSS@users.noreply.github.com>
2025-04-04 13:54:47 +0200

    Translate Name / Badge number form text

--- GCSViews/GetUserNameForm.Designer.cs
+++ GCSViews/GetUserNameForm.Designer.cs
@@ -35,26 +35,29 @@
             // 
             // tb_Name
             // 
+            this.tb_Name.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tb_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tb_Name.Location = new System.Drawing.Point(177, 12);
+            this.tb_Name.Location = new System.Drawing.Point(137, 12);
             this.tb_Name.Name = "tb_Name";
-            this.tb_Name.Size = new System.Drawing.Size(332, 26);
+            this.tb_Name.Size = new System.Drawing.Size(370, 26);
             this.tb_Name.TabIndex = 0;
             this.tb_Name.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_Name_KeyDown);
             // 
             // lb_Name
             // 
             this.lb_Name.AutoSize = true;
-            this.lb_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lb_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.lb_Name.Location = new System.Drawing.Point(12, 15);
             this.lb_Name.Name = "lb_Name";
-            this.lb_Name.Size = new System.Drawing.Size(143, 16);
+            this.lb_Name.Size = new System.Drawing.Size(119, 20);
             this.lb_Name.TabIndex = 1;
-            this.lb_Name.Text = "Name / Badge number";
+            this.lb_Name.Text = "Név / Azonosító";
             // 
             // btn_Start
             // 
-            this.btn_Start.Location = new System.Drawing.Point(217, 51);
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_Start.Location = new System.Drawing.Point(205, 53);
             this.btn_Start.Name = "btn_Start";
             this.btn_Start.Size = new System.Drawing.Size(87, 46);
             this.btn_Start.TabIndex = 2;
@@ -80,7 +83,7 @@
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Pilot Log In";
+            this.Text = "Pilóta belépés";
             this.TopMost = true;
             this.VisibleChanged += new System.EventHandler(this.GetUserNameForm_VisibleChanged);
             this.ResumeLayout(false);
