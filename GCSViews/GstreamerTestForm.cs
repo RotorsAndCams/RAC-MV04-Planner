c2c026b672471c2d6fe768f94b9928edbfe7098e
Dániel Szilágyi <szilagyi.daniel88@gmail.com>
2025-05-22 11:22:29 +0200

    Add stream ip and port input

--- GCSViews/GstreamerTestForm.cs
+++ GCSViews/GstreamerTestForm.cs
@@ -27,12 +27,31 @@ namespace MissionPlanner.GCSViews
         private readonly object _bgimagelock = new object();
         private void TestGstreamer()
         {
+            IPAddress streamIP = GetMV04SettingsStreamAddress();
+            string inputIp = streamIP.ToString();
+            if (InputBox.Show("Stream IP cím", "Add meg a stream IP címet", ref inputIp) == DialogResult.OK
+                && IPAddress.TryParse(inputIp, out IPAddress _streamIP))
+            {
+                streamIP = _streamIP;
+            }
+            else
+            {
+                CustomMessageBox.Show("Stream IP cím beállítás sikertelen");
+            }
+
+            int streamPort = GetMV04SettingsStreamPort();
+            string inputPort = streamPort.ToString();
+            if (InputBox.Show("Stream port", "Add meg a stream portot", ref inputPort) == DialogResult.OK
+                && int.TryParse(inputPort, out int _streamPort))
+            {
+                streamPort = _streamPort;
+            }
+            else
+            {
+                CustomMessageBox.Show("Stream port beállítás sikertelen");
+            }
 
             //rtspsrc location=rtsp://192.168.0.203:554/live0 latency=0 ! decodebin ! autovideosink sync=false queue max-size-buffers=1 max-size-bytes=0 max-size-time=0
-
-            string ip;
-            string port;
-                                                   //ip        port           port
             string url = "rtpsrc location='rtp://192.168.71.100:11026/live0' port=11026 ! application/x-rtp,media=video,encoding-name=MP2T,clock-rate=90000,payload=33 ! rtpjitterbuffer latency=300 ! rtpmp2tdepay ! decodebin ! autovideosink";//@"videotestsrc pattern=pinwheel ! video/x-raw, width=1280, height=720, framerate=30/1 ! videoconvert ! video/x-raw,format=BGRA ! appsink name=outsink";
 
             try
