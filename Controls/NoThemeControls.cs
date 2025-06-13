da6ee74e5436d0ba36403cadd276f205525e1f2e
Szilágyi Dániel <17363065+LEGIONBOSS@users.noreply.github.com>
2025-06-13 14:04:21 +0200

    Add zoom control buttons to camera full screen form (#66)

---
+++ Controls/NoThemeControls.cs
@@ -0,0 +1,11 @@
+﻿using MissionPlanner.Utilities;
+using System.Windows.Forms;
+
+namespace MissionPlanner.Controls
+{
+    [PreventTheming]
+    public class FormNoTheme : Form { }
+
+    [PreventTheming]
+    public class ButtonNoTheme : Button { }
+}
