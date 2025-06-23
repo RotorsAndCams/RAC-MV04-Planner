7d246d5d7c439b001061d12ea2ab4dde1d0b8404
Dániel Szilágyi <szilagyi.daniel88@gmail.com>
2025-06-23 15:39:59 +0200

    Update FlightSafetyForm.Designer.cs

--- Controls/FlightSafetyForm.Designer.cs
+++ Controls/FlightSafetyForm.Designer.cs
@@ -29,20 +29,20 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage_Buttons = new System.Windows.Forms.TabPage();
+            this.tabPage_Functions = new System.Windows.Forms.TabPage();
             this.flowLayoutPanel_Buttons = new System.Windows.Forms.FlowLayoutPanel();
+            this.button_FlightPlanCheck = new System.Windows.Forms.Button();
             this.tabPage_ParamSet = new System.Windows.Forms.TabPage();
             this.flowLayoutPanel_ParamSet = new System.Windows.Forms.FlowLayoutPanel();
-            this.button_FlightPlanCheck = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
-            this.tabPage_Buttons.SuspendLayout();
+            this.tabPage_Functions.SuspendLayout();
             this.flowLayoutPanel_Buttons.SuspendLayout();
             this.tabPage_ParamSet.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage_Buttons);
+            this.tabControl1.Controls.Add(this.tabPage_Functions);
             this.tabControl1.Controls.Add(this.tabPage_ParamSet);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
@@ -52,16 +52,16 @@
             this.tabControl1.Size = new System.Drawing.Size(804, 461);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage_Buttons
+            // tabPage_Functions
             // 
-            this.tabPage_Buttons.Controls.Add(this.flowLayoutPanel_Buttons);
-            this.tabPage_Buttons.Location = new System.Drawing.Point(4, 29);
-            this.tabPage_Buttons.Name = "tabPage_Buttons";
-            this.tabPage_Buttons.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_Buttons.Size = new System.Drawing.Size(796, 428);
-            this.tabPage_Buttons.TabIndex = 0;
-            this.tabPage_Buttons.Text = "Buttons";
-            this.tabPage_Buttons.UseVisualStyleBackColor = true;
+            this.tabPage_Functions.Controls.Add(this.flowLayoutPanel_Buttons);
+            this.tabPage_Functions.Location = new System.Drawing.Point(4, 29);
+            this.tabPage_Functions.Name = "tabPage_Functions";
+            this.tabPage_Functions.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_Functions.Size = new System.Drawing.Size(796, 428);
+            this.tabPage_Functions.TabIndex = 0;
+            this.tabPage_Functions.Text = "Functions";
+            this.tabPage_Functions.UseVisualStyleBackColor = true;
             // 
             // flowLayoutPanel_Buttons
             // 
@@ -71,6 +71,16 @@
             this.flowLayoutPanel_Buttons.Name = "flowLayoutPanel_Buttons";
             this.flowLayoutPanel_Buttons.Size = new System.Drawing.Size(790, 422);
             this.flowLayoutPanel_Buttons.TabIndex = 0;
+            // 
+            // button_FlightPlanCheck
+            // 
+            this.button_FlightPlanCheck.Location = new System.Drawing.Point(3, 3);
+            this.button_FlightPlanCheck.Name = "button_FlightPlanCheck";
+            this.button_FlightPlanCheck.Size = new System.Drawing.Size(100, 60);
+            this.button_FlightPlanCheck.TabIndex = 0;
+            this.button_FlightPlanCheck.Text = "Check Flightplan";
+            this.button_FlightPlanCheck.UseVisualStyleBackColor = true;
+            this.button_FlightPlanCheck.Click += new System.EventHandler(this.button_FlightPlanCheck_Click);
             // 
             // tabPage_ParamSet
             // 
@@ -92,16 +102,6 @@
             this.flowLayoutPanel_ParamSet.Size = new System.Drawing.Size(790, 422);
             this.flowLayoutPanel_ParamSet.TabIndex = 0;
             // 
-            // button_FlightPlanCheck
-            // 
-            this.button_FlightPlanCheck.Location = new System.Drawing.Point(3, 3);
-            this.button_FlightPlanCheck.Name = "button_FlightPlanCheck";
-            this.button_FlightPlanCheck.Size = new System.Drawing.Size(100, 60);
-            this.button_FlightPlanCheck.TabIndex = 0;
-            this.button_FlightPlanCheck.Text = "Check Flightplan";
-            this.button_FlightPlanCheck.UseVisualStyleBackColor = true;
-            this.button_FlightPlanCheck.Click += new System.EventHandler(this.button_FlightPlanCheck_Click);
-            // 
             // FlightSafetyForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -115,7 +115,7 @@
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.Text = "Flight Safety Config";
             this.tabControl1.ResumeLayout(false);
-            this.tabPage_Buttons.ResumeLayout(false);
+            this.tabPage_Functions.ResumeLayout(false);
             this.flowLayoutPanel_Buttons.ResumeLayout(false);
             this.tabPage_ParamSet.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -125,7 +125,7 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage_Buttons;
+        private System.Windows.Forms.TabPage tabPage_Functions;
         private System.Windows.Forms.TabPage tabPage_ParamSet;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel_ParamSet;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel_Buttons;
